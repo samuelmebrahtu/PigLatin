4ba72b02749296d9d19162280a58bf1f54ba213e
samuelmebrahtu <157595431+samuelmebrahtu@users.noreply.github.com>
2024-05-28 15:42:40 +0100

    final successful code

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,6 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+
 public class PigLatin {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to be translated into Pig Latin >> ");
+        String input = scanner.nextLine();
+        scanner.close();
+        System.out.println(pigLatin(input));
+    }
+
+    public static String pigLatin(String input) {
+        String[] words = input.split(" ");
+        List<String> translated = new ArrayList<>();
+        List<String> vowels = Arrays.asList("a","e","i","o","u");
+        for (String word: words) {
+            if (vowels.contains(Character.toString(word.charAt(0)))) {
+                translated.add(word+"way");
+            }
+            else {
+                int i = 0;
+                List<String> consonants = new ArrayList<>();
+                while (!vowels.contains(Character.toString(word.charAt(0)))) {
+                    consonants.add(Character.toString(word.charAt(i)));
+                    word = word.substring(i+1);
+                }
+                word = word + String.join("", consonants) + "ay";
+                translated.add(word);
+            }
+        }
+        return String.join(" ", translated);
     }
 }
